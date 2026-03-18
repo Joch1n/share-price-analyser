66971b1f93f47378ee70725057075a78f2ce0102
Amel Soumia Bouras <amelsoumia245@gmail.com>
2026-03-18 15:24:35 +0000

    Add code and update file organisation.

--- app/src/main/java/org/roehampton/presentation/UISkeleton.java
+++ app/src/main/java/org/roehampton/presentation/UISkeleton.java
@@ -2,12 +2,22 @@ package org.roehampton.presentation;
 
 public class UISkeleton implements IUserInterface {
 
+    private final ICompanySearchView companySearchView;
+    private final IWatchlistView watchlistView;
+    private final IGraphView graphView;
 
-    public UISkeleton() {
+    public UISkeleton(ICompanySearchView companySearchView, IWatchlistView watchlistView, IGraphView graphView) {
+
+        this.companySearchView = companySearchView;
+        this.watchlistView = watchlistView;
+        this.graphView = graphView;
+
     }
 
     @Override
     public void start() {
+
+        // Start server, handle routing
 
     }
 }
