e1f533c0337223f3dcacdbcc8448fca87a0dc00b
lybaa786 <lybaa611@gmail.com>
2026-03-18 23:18:25 +0000

    updated graph view

--- app/src/main/java/org/roehampton/presentation/GraphView.java
+++ app/src/main/java/org/roehampton/presentation/GraphView.java
@@ -1,4 +1,9 @@
-package org.roehampton.presentation;
+package main.java.org.roehampton.presentation;
+
+// Minimal IController interface
+interface IController {
+    void handleDataPointClick(int index, double value);
+}
 
 public class GraphView {
 
@@ -14,20 +19,5 @@ public class GraphView {
 
     public void clickDataPoint(int index, double value) {
         controller.handleDataPointClick(index, value);
-    }
-
-    public static void main(String[] args) {
-
-        IController controller = new IController() {
-            @Override
-            public void handleDataPointClick(int index, double value) {
-                System.out.println("Controller received click: index=" + index + ", value=" + value);
-            }
-        };
-
-        GraphView view = new GraphView(controller);
-
-        view.showMessage("Hello, Graph!");
-        view.clickDataPoint(1, 42.0);
     }
 }
